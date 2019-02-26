94c67d061f7829842b72901a0da3266109c6614f
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-26 17:22:56 +0900

    #2.7 DOM - If else - Function practice part Two

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 const title = document.querySelector("#title")
 
-const BASE_COLOR = "rgb(52, 73, 94)"
-const OTHER_COLOR = "#7f8c8d"
+const CLICKED_CLASS = "clicked"
 
 function handleClick() {
-  const currentColor = title.style.color
-  console.log(currentColor)
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR
-  } else {
-    title.style.color = BASE_COLOR
-  }
+  // const hasClass = title.classList.contains(CLICKED_CLASS)
+  // if (hasClass) {
+  //   title.classList.remove(CLICKED_CLASS)
+  // } else {
+  //   title.classList.add(CLICKED_CLASS)
+  // }
+  title.classList.toggle(CLICKED_CLASS)
 }
 
 function init() {
-  title.style.color = BASE_COLOR
-  title.addEventListener("mouseenter", handleClick)
+  title.addEventListener("click", handleClick)
 }
 
 init()
-
-function handleOffline() {
-  console.log("bye bye")
-}
-
-function handleOnline() {
-  console.log("welcome back")
-}
-
-window.addEventListener("offline", handleOffline)
-window.addEventListener("online", handleOnline)
