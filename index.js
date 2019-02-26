7d80d8226d269da438c07afe47ad715cdfe0ba65
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-26 15:22:21 +0900

    #2.3 Modifying the DOM with JS

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // document 역시 오브젝트
-const title = document.getElementById("title")
+const title = document.querySelector("#title")
+// id로 찾으면 #title, class로 찾으면 .title
 title.innerHTML = "Hi From JS"
-console.log(title)
+title.style.color = "red"
+document.title = "I owe you"
+console.dir(document)
 
 // DOM : Document Object Module
