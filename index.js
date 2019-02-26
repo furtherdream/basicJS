c7f9a6a0b2cc736f4b4752d63b2cadf7ffde027b
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-26 16:19:07 +0900

    #2.6 DOM - If else - Function practice

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const age = prompt("How old are you")
+const title = document.querySelector("#title")
 
-if (age >= 18 && age <= 21) {
-  console.log("you can drink but you should not")
-} else if (age > 21) {
-  console.log("go ahed")
-} else {
-  console.log("too young")
+const BASE_COLOR = "rgb(52, 73, 94)"
+const OTHER_COLOR = "#7f8c8d"
+
+function handleClick() {
+  const currentColor = title.style.color
+  console.log(currentColor)
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR
+  } else {
+    title.style.color = BASE_COLOR
+  }
 }
+
+function init() {
+  title.style.color = BASE_COLOR
+  title.addEventListener("mouseenter", handleClick)
+}
+
+init()
+
+function handleOffline() {
+  console.log("bye bye")
+}
+
+function handleOnline() {
+  console.log("welcome back")
+}
+
+window.addEventListener("offline", handleOffline)
+window.addEventListener("online", handleOnline)
